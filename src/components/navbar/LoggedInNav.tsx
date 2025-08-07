3583dce6e0aa7a9552e43b64626d3566fb29cfb9
Egi <satria.pandega@gmail.com>
2025-08-07 12:26:28 +0700

    test

--- src/components/navbar/LoggedInNav.tsx
+++ src/components/navbar/LoggedInNav.tsx
@@ -15,9 +15,11 @@ const LoggedInNav: React.FC<NavbarLoggedInProps> = ({ onLogout }) => {
   const [notificationDropdownOpen, setNotificationDropdownOpen] =
     useState(false);
   const [notificationCount, setNotificationCount] = useState(0);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
   const notificationRef = useRef<HTMLDivElement>(null);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const fetchNotificationCount = async () => {
@@ -44,10 +46,23 @@ const LoggedInNav: React.FC<NavbarLoggedInProps> = ({ onLogout }) => {
       ) {
         setNotificationDropdownOpen(false);
       }
+      if (
+        mobileMenuRef.current &&
+        !mobileMenuRef.current.contains(event.target as Node)
+      ) {
+        setMobileMenuOpen(false);
+      }
     }
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+    setDropdownOpen(false);
+    setNotificationDropdownOpen(false);
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -72,11 +87,341 @@ const LoggedInNav: React.FC<NavbarLoggedInProps> = ({ onLogout }) => {
   return (
     <div className="sticky top-0 z-10 shadow-md">
       <nav className="bg-link-nav p-4 flex justify-between items-center relative">
-        <div className="flex items-center space-x-4 gap-8">
-          <a href="/" className="text-3xl font-bold text-title">
+        <div className="flex items-center md:hidden">
+          <a href="/" className="text-3xl font-bold text-title mr-4">
             Arsipku
           </a>
-          <div className="relative">
+        </div>
+
+        {/* Desktop menu */}
+        <div className="hidden md:flex items-center space-x-4 gap-8 justify-between w-full">
+          <div className="flex items-center space-x-4">
+            <a href="/" className="text-3xl font-bold text-title mr-4">
+              Arsipku
+            </a>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Cari"
+                className="w-full pl-3 pr-10 p-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-accent"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+              />
+              {inputValue && (
+                <button
+                  type="button"
+                  className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-700"
+                  onClick={handleClearSearch}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              )}
+            </div>
+            <button
+              type="button"
+              onClick={handleSearch}
+              className="text-gray-400 hover:text-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-4.35-4.35M16 10a6 6 0 11-12 0 6 6 0 0112 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link
+              to="/buat-arsip"
+              className="text-white hover:text-text-main mr-4"
+            >
+              Buat arsip
+            </Link>
+            <Link
+              to="/arsip"
+              className="text-white hover:text-text-main mr-4"
+              onClick={handleClearSearch}
+            >
+              List Arsip
+            </Link>
+            <Link
+              to="/dashboard"
+              className="text-white hover:text-text-main mr-4"
+            >
+              Dashboard
+            </Link>
+
+            <div className="relative flex items-center" ref={notificationRef}>
+              <div className="relative mr-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-[40px] w-[40px] text-white cursor-pointer hover:text-accent transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  onClick={() =>
+                    setNotificationDropdownOpen(!notificationDropdownOpen)
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+                {notificationCount > 0 && (
+                  <span className="absolute bottom-6 left-6 -mt-1 -mr-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-600 text-xs font-bold text-white shadow-lg">
+                    {notificationCount}
+                  </span>
+                )}
+                {notificationDropdownOpen && (
+                  <div className="absolute right-0 top-full mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
+                    <Link
+                      to="/list-verifier"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      onClick={() => setNotificationDropdownOpen(false)}
+                    >
+                      List Verifier
+                    </Link>
+                    <Link
+                      to="/list-appliance"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      onClick={() => setNotificationDropdownOpen(false)}
+                    >
+                      List Appliance
+                    </Link>
+                  </div>
+                )}
+              </div>
+
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="bg-button-highlight-blue hover:bg-accent text-white font-bold py-2 px-4 rounded flex items-center gap-2"
+              >
+                Akun
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </button>
+
+              {dropdownOpen && (
+                <div className="absolute right-0 top-full mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
+                  <Link
+                    to="/profile"
+                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    onClick={() => {
+                      setDropdownOpen(false);
+                      toggleMobileMenu();
+                    }}
+                  >
+                    Profile
+                  </Link>
+                  <button
+                    onClick={() => {
+                      setDropdownOpen(false);
+                      onLogout();
+                    }}
+                    className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                  >
+                    Keluar
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* Mobile menu button */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMobileMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {mobileMenuOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </nav>
+
+      {/* Mobile menu */}
+      {mobileMenuOpen && (
+        <div
+          className="md:hidden bg-link-nav fixed top-0 left-0 w-full h-screen z-50"
+          ref={mobileMenuRef}
+        >
+          <div className="flex flex-col items-center gap-4 p-4">
+            <div className="flex justify-between w-full">
+              <div className="relative mr-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-[40px] w-[40px] text-white cursor-pointer hover:text-accent transition-colors duration-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  onClick={() =>
+                    setNotificationDropdownOpen(!notificationDropdownOpen)
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+                {notificationCount > 0 && (
+                  <span className="absolute bottom-6 left-6 -mt-1 -mr-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-600 text-xs font-bold text-white shadow-lg">
+                    {notificationCount}
+                  </span>
+                )}
+                {notificationDropdownOpen && (
+                  <div className="absolute right-0 top-full mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
+                    <Link
+                      to="/list-verifier"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      onClick={() => {
+                        setNotificationDropdownOpen(false);
+                        toggleMobileMenu();
+                      }}
+                    >
+                      List Verifier
+                    </Link>
+                    <Link
+                      to="/list-appliance"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      onClick={() => {
+                        setNotificationDropdownOpen(false);
+                        toggleMobileMenu();
+                      }}
+                    >
+                      List Appliance
+                    </Link>
+                  </div>
+                )}
+              </div>
+              <button
+                onClick={toggleMobileMenu}
+                className="text-white focus:outline-none self-start"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <Link
+              to="/buat-arsip"
+              className="text-white hover:text-text-main"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              Buat arsip
+            </Link>
+            <Link
+              to="/arsip"
+              className="text-white hover:text-text-main"
+              onClick={() => {
+                handleClearSearch();
+                toggleMobileMenu();
+              }}
+            >
+              List arsip
+            </Link>
+            <Link
+              to="/dashboard"
+              className="text-white hover:text-text-main"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              Dashboard
+            </Link>
+            <Link
+              to="/profile"
+              className="text-white hover:text-text-main"
+              onClick={() => {
+                setDropdownOpen(false);
+                toggleMobileMenu();
+              }}
+            >
+              Profile
+            </Link>
+            <button
+              onClick={() => {
+                setDropdownOpen(false);
+                onLogout();
+              }}
+              className="text-white hover:text-text-main"
+            >
+              Keluar
+            </button>
+          </div>
+          <div className="flex justify-between absolute w-11/12 px-4 bottom-6">
             <input
               type="text"
               placeholder="Cari"
@@ -107,139 +452,29 @@ const LoggedInNav: React.FC<NavbarLoggedInProps> = ({ onLogout }) => {
                 </svg>
               </button>
             )}
-          </div>
-          <button
-            type="button"
-            onClick={handleSearch}
-            className="text-gray-400 hover:text-gray-700"
-            style={{ marginLeft: "-20px" }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-4.35-4.35M16 10a6 6 0 11-12 0 6 6 0 0112 0z"
-              />
-            </svg>
-          </button>
-        </div>
-
-        <div className="flex items-center gap-8 relative" ref={dropdownRef}>
-          <Link
-            to="/buat-arsip"
-            className="text-white hover:text-text-main mr-4"
-          >
-            Buat arsip
-          </Link>
-          <Link
-            to="/arsip"
-            className="text-white hover:text-text-main mr-4"
-            onClick={handleClearSearch}
-          >
-            List arsip
-          </Link>
-          <Link
-            to="/dashboard"
-            className="text-white hover:text-text-main mr-4"
-          >
-            Dashboard
-          </Link>
-
-          <div className="relative flex items-center" ref={notificationRef}>
-            <div className="relative mr-8">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-[40px] w-[40px] text-white cursor-pointer hover:text-accent transition-colors duration-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-                onClick={() =>
-                  setNotificationDropdownOpen(!notificationDropdownOpen)
-                }
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              {notificationCount > 0 && (
-                <span className="absolute bottom-6 left-6 -mt-1 -mr-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-600 text-xs font-bold text-white shadow-lg">
-                  {notificationCount}
-                </span>
-              )}
-              {notificationDropdownOpen && (
-                <div className="absolute right-0 top-full mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
-                  <Link
-                    to="/list-verifier"
-                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                    onClick={() => setNotificationDropdownOpen(false)}
-                  >
-                    List Verifier
-                  </Link>
-                  <Link
-                    to="/list-appliance"
-                    className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                    onClick={() => setNotificationDropdownOpen(false)}
-                  >
-                    List Appliance
-                  </Link>
-                </div>
-              )}
-            </div>
-
             <button
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-              className="bg-button-highlight-blue hover:bg-accent text-white font-bold py-2 px-4 rounded flex items-center gap-2"
+              type="button"
+              onClick={handleSearch}
+              className="relative text-gray-400 hover:text-gray-700 left-8"
             >
-              Akun
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="h-8 w-8"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                strokeWidth={2}
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M19 9l-7 7-7-7"
+                  strokeWidth="2"
+                  d="M21 21l-4.35-4.35M16 10a6 6 0 11-12 0 6 6 0 0112 0z"
                 />
               </svg>
             </button>
-
-            {dropdownOpen && (
-              <div className="absolute right-0 top-full mt-2 w-40 bg-white rounded-md shadow-lg py-2 z-20">
-                <Link
-                  to="/profile"
-                  className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                  onClick={() => setDropdownOpen(false)}
-                >
-                  Profile
-                </Link>
-                <button
-                  onClick={() => {
-                    setDropdownOpen(false);
-                    onLogout();
-                  }}
-                  className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Keluar
-                </button>
-              </div>
-            )}
           </div>
         </div>
-      </nav>
+      )}
     </div>
   );
 };
